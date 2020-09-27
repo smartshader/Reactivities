0cf099738805c9f12b43e9c2867f822f259f5630
Imad Jerra <smartshader@gmail.com>
2020-09-26 20:06:00 -0400

    Adding loading indicator

--- API/client-app/src/app/layout/App.tsx
+++ API/client-app/src/app/layout/App.tsx
@@ -4,12 +4,14 @@ import { IActivity } from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 const App = () => {
   
   const [activities, setActivities] = useState<IActivity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
   
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities.filter(activity => activity.id === id)[0]);
@@ -52,8 +54,10 @@ const App = () => {
           activities.push(activity);
         });
         setActivities(activities);
-      });
+      }).then(() => setLoading(false));
   }, []);
+  
+  if (loading) return <LoadingComponent inverted={true} content='Loading activities...' />
 
   return (
     <Fragment>
